86f56ace1e3e95013e5021615b724fd05054d9c1
brenoportella_ <bp.bohry@gmail.com>
2024-07-17 16:52:55 +0100

    fix pakages' names

--- backupsaftonline/main.py
+++ backupsaftonline/main.py
@@ -1,22 +1,25 @@
 import time
-import credentials
+import backupsaftonline.credentials as credentials
 import pandas as pd
-from login import login
-from scrapy_nif import scrapy_nif
-from driver import setup_driver as driver
+from backupsaftonline.login import login
+from backupsaftonline.scrapy_nif import scrapy_nif
+from backupsaftonline.driver import setup_driver as driver
 
 email = credentials.email
 password = credentials.password
 
 class Backup:
-    def main():
+    def __init__(self):
+        self.driver = driver()
+
+    def core(self):
         
-        login(driver, email, password)
-        info = scrapy_nif(driver)
+        login(self.driver, email, password)
+        info = scrapy_nif(self.driver)
     
         driver.quit()
 
         df = pd.DataFrame(info)
         process_date = time.strftime("%d-%m-%Y")
         xlsx_name = f"backup-saft-{process_date}.xlsx"
-        df.to_excel(xlsx_name, index=False)
+        df.to_excel(xlsx_name, index=False)    
