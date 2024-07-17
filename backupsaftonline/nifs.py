83223dbc946b87ee6ff7d550c4b4d2ed6dd44c27
brenoportella_ <bp.bohry@gmail.com>
2024-07-17 16:55:00 +0100

    fix pakages' names and click func

--- backupsaftonline/nifs.py
+++ backupsaftonline/nifs.py
@@ -1,4 +1,4 @@
 def nifs():
-    with open('../tests/nif_satf.txt', 'r') as file:
+    with open('./tests/nif_saft.txt', 'r') as file:
         nifs = [line.strip() for line in file.readlines()]
     return nifs
--- backupsaftonline/scrapy_nif.py
+++ backupsaftonline/scrapy_nif.py
@@ -1,21 +1,26 @@
-from search_nif import search_nif
-from details_fields import details_fields
-from accounts_fields import account_fields
-from extract_info import extract_info
-from nifs import nifs
+from backupsaftonline.search_nif import search_nif
+from backupsaftonline.details_fields import details_fields
+from backupsaftonline.accounts_fields import account_fields
+from backupsaftonline.extract_info import extract_info
+from backupsaftonline.nifs import nifs
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 def scrapy_nif(driver):
     info_list = []
-
-    for nif in nifs:
+    my_nifs = nifs()
+    for nif in my_nifs:
         search_nif(driver, nif)
         for field in details_fields:
             details = extract_info(driver, field)
 
-        bt_conta = driver.find_element(By.ID, "Contas-tab")
-        bt_conta.click()
+        bt_conta = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID,"Contas-tab"))
+        )
+        driver.execute_script("arguments[0].scrollIntoView(true);", bt_conta)
+        driver.execute_script("arguments[0].click();", bt_conta)
 
         for field in account_fields:
             accounts = extract_info(driver, field)
