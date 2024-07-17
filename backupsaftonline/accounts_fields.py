3dfd868af6a9e945987e0a38051a14a966041ff2
brenoportella_ <bp.bohry@gmail.com>
2024-07-17 10:30:05 +0100

    add fields

---
+++ backupsaftonline/accounts_fields.py
@@ -0,0 +1,8 @@
+account_fields =[
+    "Empresa.API_UsernameAT", "Empresa.API_PasswordAT", "Empresa.API_UsernameSS", "Empresa.API_PasswordSS",
+    "Empresa.API_UsernameVIACTT", "Empresa.API_PasswordVIACTT", "Empresa.API_UsernameIAPMEI", "Empresa.API_PasswordIAPMEI",
+    "Empresa.API_UsernameRU", "Empresa.API_PasswordRU", "Empresa.API_UsernameIEFP", "Empresa.API_PasswordIEFP",
+    "Empresa.API_UsernameB2020", "Empresa.API_PasswordB2020", "Empresa.API_UsernameINE", "Empresa.API_PasswordINE",
+    "Empresa.API_UsernameSILIAMB", "Empresa.API_PasswordSILIAMB","Empresa.API_UsernameLivroRec", "Empresa.API_PasswordLivroRec",
+    "Empresa.API_UsernameACT", "Empresa.API_PasswordACT"
+]
