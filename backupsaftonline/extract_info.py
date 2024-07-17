38846af5d25c72f9ff4f3eec9e42b566175e3f28
brenoportella_ <bp.bohry@gmail.com>
2024-07-17 11:38:20 +0100

    add new extract info

---
+++ backupsaftonline/extract_info.py
@@ -0,0 +1,20 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+import time
+
+def extract_conta(driver, field_name):
+    info_dict = {}
+    time.sleep(1)
+
+    try:
+        field= WebDriverWait(driver,20).until(
+          EC.presence_of_element_located((By.NAME, field_name))
+        )
+        field_value = field.get_attribute("value")
+        info_dict[field_name] = field_value
+    except NoSuchElementException:
+        info_dict[field_name] = "N/A"
+      
+    return info_dict
