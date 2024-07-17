1884ef88372917d81111c044b34205cbab60e396
brenoportella_ <bp.bohry@gmail.com>
2024-07-17 16:52:01 +0100

    remove timesleep

--- backupsaftonline/extract_info.py
+++ backupsaftonline/extract_info.py
@@ -2,12 +2,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
-import time
 
 def extract_info(driver, field_name):
     info_dict = {}
-    time.sleep(1)
-
     try:
         field= WebDriverWait(driver,20).until(
           EC.presence_of_element_located((By.NAME, field_name))
