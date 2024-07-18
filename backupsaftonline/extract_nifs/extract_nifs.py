6b1153aa59d8445d9225a4c6936124c1916c581e
brenoportella_ <bp.bohry@gmail.com>
2024-07-18 16:50:56 +0100

    add extract nif funcs

---
+++ backupsaftonline/extract_nifs/extract_nifs.py
@@ -0,0 +1,32 @@
+# ok, basicamente depois do login, devemos extrair o excel.
+'''só que para baixar o arquivo, devemos configurar o path to download do browser,
+o que pode ser o path do .\resulsts
+
+depois disso precisamos acessar o arquivo, gerar um dataframe do xlsx, salvar em um 
+txt com o nome nif_saft.txt substituindo o ficheiro antigo, apagar o arquivo xlsx
+e a partir dai vamos para iniciar o programa como sempre'''
+
+from backupsaftonline.driver import setup_driver as driver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import pandas as pd
+import os
+
+def download_nifs(driver):
+
+    download_bt = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, 'img-responsive.center-block'))
+    )
+    download_bt.click()
+
+def read_xlsx(path, file):
+    file_path = os.path.join(path, file)
+    df = pd.read_excel(file_path, skiprows = 1, usecols=[0])
+
+    output = os.path.join(path, "nif_saft.txt")
+    df.to_csv(output, sep='\t', index=False)
+
+def delete_file(file):
+    os.remove(file)
