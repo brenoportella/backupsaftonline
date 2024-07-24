816481989b66c93f8dd34cbc0f9fae9343ca043a
brenoportella_ <bp.bohry@gmail.com>
2024-07-24 15:39:11 +0100

    add interface modules

--- backupsaftonline/interface/interface.py
+++ backupsaftonline/interface/interface.py
@@ -1,2 +1,14 @@
+from backupsaftonline.interface.frames import frames
+from backupsaftonline.interface.widgets import options_menu, widgets
+
+
 class Interface:
-    pass
+    def __init__(self, root):
+        self.root = root
+        self.root.title('Backup SaftOnline')
+        self.root.geometry('480x648')
+        self.root.resizable(0, 0)
+
+        self.frames = frames(self)
+        self.widgets = widgets(self)
+        self.options_menu = options_menu(self)
--- backupsaftonline/interface/interface_func.py
+++ backupsaftonline/interface/interface_func.py
@@ -1,2 +1,24 @@
+import os
+from tkinter import messagebox
+from backupsaftonline.credentials import save_credentials
+import backupsaftonline.main as bp
+
 class InterfaceFunc:
-    pass
+    def open_pdf_with_edge(self):
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(current_dir, 'GUIA BPSAFTONLINE.pdf')
+        if file_path:
+            os.system(f'start msedge "{file_path}"')
+
+    def contact(self):
+        messagebox.showinfo(
+            'Contacto', 'Envie um e-mail para breno.portella@bwv.pt'
+        )
+
+    def start_backup(self):
+        email = self.login_entry.get()
+        password = self.pwd_entry.get()
+        save_credentials(email, password)  # Salvar credenciais
+
+        backup = bp.Backup()
+        backup.core()
