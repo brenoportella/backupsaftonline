e0f94821c57113914b964bb21f7ae669c9673995
brenoportella_ <bp.bohry@gmail.com>
2024-07-24 15:30:38 +0100

    add visual elements gui

---
+++ backupsaftonline/interface/frames.py
@@ -0,0 +1,6 @@
+import customtkinter as ctk
+from backupsaftonline.interface.colors import *
+
+def frames(self):
+    self.frame_1 = ctk.CTkFrame(self.root, fg_color= branco)
+    self.frame_1.place(relx=0, rely=0, relheight=1, relwidth=1)
