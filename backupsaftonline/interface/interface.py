e03e901774e4df9fa31367b641bf193af64e0206
brenoportella_ <bp.bohry@gmail.com>
2024-08-01 17:19:51 +0100

    add interfacefunc start and start process on thread

--- backupsaftonline/interface/interface.py
+++ backupsaftonline/interface/interface.py
@@ -1,4 +1,7 @@
+import threading
+
 from backupsaftonline.interface.frames import Frames
+from backupsaftonline.interface.interface_func import InterfaceFunc
 from backupsaftonline.interface.widgets import Widgets
 
 
@@ -9,6 +12,15 @@ class Interface:
         self.root.geometry('480x648')
         self.root.resizable(0, 0)
 
+        self.interface_func = InterfaceFunc(self)
         self.frames = Frames.frames(self, root)
         self.widgets = Widgets.widgets(self)
         self.widgets = Widgets.options_menu(self)
+
+    def run_start_backup(self):
+        self.interface_func.run_start_backup()
+
+
+# CASO PRECISE ATUALIZAR A UI USAR ESSE METODO
+# def update_ui_callback(self, message):
+#     print(message)
