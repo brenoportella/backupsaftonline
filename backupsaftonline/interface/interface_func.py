25ba372c6d143359f1713f48ae3c76c76edc5be7
brenoportella_ <bp.bohry@gmail.com>
2024-08-01 17:18:56 +0100

    add threading and interface context arg

--- backupsaftonline/interface/interface_func.py
+++ backupsaftonline/interface/interface_func.py
@@ -1,9 +1,15 @@
 import os
+import threading
 from tkinter import messagebox
-from backupsaftonline.credentials import save_credentials
+
 import backupsaftonline.main as bp
+from backupsaftonline.credentials import save_credentials
+
 
 class InterfaceFunc:
+    def __init__(self, interface):
+        self.interface = interface
+
     def open_pdf_with_edge(self):
         current_dir = os.path.dirname(os.path.abspath(__file__))
         file_path = os.path.join(current_dir, 'GUIA BPSAFTONLINE.pdf')
@@ -16,9 +22,15 @@ class InterfaceFunc:
         )
 
     def start_backup(self):
-        email = self.login_entry.get()
-        password = self.pwd_entry.get()
+        email = self.interface.login_entry.get()
+        password = self.interface.pwd_entry.get()
         save_credentials(email, password)  # Salvar credenciais
 
         backup = bp.Backup()
         backup.core()
+
+        # Após o backup ser concluído, atualize a UI
+        # self.interface.update_ui_callback('Backup completed!')
+
+    def run_start_backup(self):
+        threading.Thread(target=self.start_backup).start()
